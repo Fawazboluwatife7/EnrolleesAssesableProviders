1a2e9a7a7b46b2bc3f02abf8e8b5114c0152ed77
Fawazboluwatife7 <147808555+Fawazboluwatife7@users.noreply.github.com>
2025-06-10 23:33:57 +0100

    final fixx

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -705,7 +705,7 @@ const Homepage = () => {
                 {FilteredProviders?.length > itemsPerPage && (
                     <div className="flex justify-center mt-4 items-center gap-4">
                         <button
-                            className="px-4 py-2 mx-1 bg-white text-red-600 border border-red-600 rounded-md flex sm:w-[2rem] md:w-[10rem] "
+                            className="px-4 py-2 mx-1 bg-white text-red-600 border border-red-600 rounded-md flex sm:w-[5rem] md:w-[2rem] "
                             disabled={currentPage === 1}
                             onClick={() =>
                                 setCurrentPage((prev) => Math.max(prev - 1, 1))
@@ -721,7 +721,7 @@ const Homepage = () => {
                         </span>
 
                         <button
-                            className="px-4 py-2 mx-1 bg-white text-red-600 border border-red-600 rounded-md flex sm:w-[2rem] md:w-[10rem] "
+                            className="px-4 py-2 mx-1 bg-white text-red-600 border border-red-600 rounded-md flex sm:w-[5rem] md:w-[2rem] "
                             disabled={currentPage >= totalPages}
                             onClick={() => setCurrentPage((prev) => prev + 1)}
                         >
